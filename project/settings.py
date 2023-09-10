e50b41161ce1d1d0b652e1a8b1c557fe27f2c075
i_boradyn <igor.boradyn@gmail.com>
2023-09-10 13:31:24 +0200

    Removed email configuration.

--- project/settings.py
+++ project/settings.py
@@ -149,16 +149,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'apps_static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-# Email
-EMAIL_BACKEND = env.str('EMAIL_BACKEND', '')
-DEFAULT_FROM_EMAIL = env.str('DEFAULT_FROM_EMAIL', '')
-EMAIL_HOST = env.str('EMAIL_HOST', '')
-EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', '')
-EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', '')
-EMAIL_PORT = env.str('EMAIL_PORT', '')
-EMAIL_HOST_USER = env.str('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD', '')
-
 # Celery
 CELERY = {
     'BROKER_URL': env.str('REDIS_URL'),
