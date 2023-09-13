d42d485f765d4dc4b36c1833c41a77c42ba9788f
i_boradyn <igor.boradyn@gmail.com>
2023-09-13 22:59:09 +0200

    Base template refactor. Added sidebar for applications pages.

---
+++ static/js/sidebar.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebarCollapseIcon = document.querySelector('#header_collapse_icon i');
+    sidebarCollapseIcon.addEventListener('click', toggleSidebarPop);
+
+    showActiveSidebarLink();
+});
+
+const toggleSidebarPop = () => {
+    document.querySelector('.side-header').classList.toggle('header-pop');
+}
+
+const showActiveSidebarLink = () => {
+    const navNumber = document.querySelector('#active_nav_link_number').innerHTML;
+
+    if (navNumber) {
+        const navLink = document.querySelector(`.nav-list .nav-item:nth-child(${navNumber}) .nav-link`);
+        navLink.classList.add('active');
+        navLink.setAttribute('aria-current', 'page');
+    }
+}
---
+++ static/js/subNavList.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navItemsCollapseIcon = document.querySelector('.sub-items-collapse-icon');
+
+    setSubNavListsHeights();
+
+    navItemsCollapseIcon.addEventListener('click', (e) => {
+        toggleSubNavList(e.currentTarget);
+    });
+
+    showActiveSidebarSubLink();
+});
+
+const setSubNavListsHeights = () => {
+    const subNavLists = document.querySelectorAll('.sub-nav-list');
+
+    subNavLists.forEach((elem) => {
+        elem.style.transition = 'None';
+        elem.style.maxHeight = 'None';
+        elem.dataset.height = `${elem.offsetHeight}px`;
+        elem.style.maxHeight = null;
+        elem.style.transition = null;
+    });
+}
+
+const showSubNavList = (icon, subNavList, animation) => {
+    const height = subNavList.dataset.height;
+
+    subNavList.style.borderTop = '1px dashed #000';
+    subNavList.style.maxHeight = height;
+
+    if(animation) {
+        icon.classList.remove('rotate-down');
+        icon.classList.remove('rotate-down-no-animation');
+        icon.classList.add('rotate-up');
+    } else {
+        icon.classList.remove('rotate-down');
+        icon.classList.remove('rotate-down-no-animation');
+        icon.classList.add('rotate-up-no-animation');
+    }
+}
+
+const closeSubNavList = (icon, subNavList, animation) => {
+    subNavList.style.maxHeight = null;
+
+    if(animation) {
+        icon.classList.remove('rotate-up');
+        icon.classList.remove('rotate-up-no-animation');
+        icon.classList.add('rotate-down');
+    } else {
+        icon.classList.remove('rotate-up');
+        icon.classList.remove('rotate-up-no-animation');
+        icon.classList.add('rotate-down-no-animation');
+    }
+
+    setTimeout(() => {
+        subNavList.style.borderTop = 'None';
+    }, 200);
+}
+
+const toggleSubNavList = (navList, animation=true) => {
+    const icon = navList.children[0];
+    const subNavList = navList.parentElement.nextElementSibling;
+
+    if(subNavList.style.maxHeight){
+        closeSubNavList(icon, subNavList, animation);
+    } else {
+        showSubNavList(icon, subNavList, animation);
+    }
+}
+
+const showActiveSidebarSubLink = () => {
+    const subNavSelector = document.querySelector('#active_sub_nav_link_selector').innerHTML.trim().split(',');
+
+    if (subNavSelector) {
+        const navLink = document.querySelector(`#${subNavSelector[0]} .sub-item:nth-child(${subNavSelector[1]}) .sub-link`);
+
+        navLink.parentElement.parentElement.previousElementSibling.querySelector('.nav-link').classList.add('active');
+        navLink.classList.add('active');
+        navLink.setAttribute('aria-current', 'page');
+
+        const icon = navLink.parentElement.parentElement.previousElementSibling.querySelector('.sub-items-collapse-icon');
+
+        toggleSubNavList(icon, false);
+    }
+}
